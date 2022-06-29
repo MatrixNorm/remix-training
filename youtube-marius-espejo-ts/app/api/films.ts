82137eb4b940c40587f4f32b94ca7b0f82bbc0d1
Ky Ky <kyky@kyky.ky>
2022-06-29 23:30:27 +0300

    *

--- youtube-marius-espejo-ts/app/api/films.ts
+++ youtube-marius-espejo-ts/app/api/films.ts
@@ -1,19 +1,15 @@
-import { z } from "zod";
+import type { Character, Film } from "./restEndpoints";
+import Ajv from "ajv";
+import * as filmRestApiSchema from "./schema/film.json";
+import * as characterRestApiSchema from "./schema/character.json";
 import { dbRead } from "./utils";
 import { getCommentsByFilmId } from "./comments";
 
-const Film = z.object({
-  id: z.string(),
-  title: z.string(),
-  image: z.string(),
-  movie_banner: z.string(),
-  description: z.string(),
-  release_date: z.string(),
-  characters: z.array(Characters),
-  comments: z.array(Comments),
-});
-
-type Film = z.infer<typeof Film>;
+const ajv = new Ajv();
+const validator = {
+  film: ajv.compile(filmRestApiSchema),
+  character: ajv.compile(characterRestApiSchema),
+};
 
 async function readFilms() {
   /* алиас ~ резолвится на этапе транспиляции, поэтому нельзя
@@ -24,39 +20,56 @@ async function readFilms() {
       }
       read("~/data/films")    
   */
-  return await dbRead(require("~/data/films"));
+  const externalData = await dbRead(require("~/data/films"));
+  const goodEntries = Object.entries(externalData).filter(([filmId, filmExt]) =>
+    validator.film(filmExt)
+  );
+  return Object.fromEntries(goodEntries) as {
+    [k: string]: Film;
+  };
 }
 
 async function readCharacters() {
-  return await dbRead(require("~/data/characters"));
+  const externalData = await dbRead(require("~/data/characters"));
+  const goodEntries = Object.entries(externalData).filter(([charId, charExt]) =>
+    validator.character(charExt)
+  );
+  return Object.fromEntries(goodEntries) as {
+    [k: string]: Character;
+  };
 }
 
-export const getFilms = async (title) => {
+export const getFilms = async (title: string) => {
   /*XXX spec
     загружаем данные из непроверенного источника
     надо проверять данные на соответствие спецификации
     смотри clojure spec
   */
   const films = Object.values(await readFilms());
-  return films.filter((film) =>
-    title ? film.title.toLowerCase().includes(title.toLowerCase()) : true
-  );
+  if (title) {
+    return films.filter((film) => film.title.toLowerCase().includes(title.toLowerCase()));
+  }
+  return films;
 };
 
-export const getFilmById = async (filmId) => {
+export const getFilmById = async (filmId: string) => {
   const films = await readFilms();
   const characters = await readCharacters();
   const comments = await getCommentsByFilmId(filmId);
   const film = films[filmId];
-  return { ...film, characters: film.characters.map((pId) => characters[pId]), comments };
+  return {
+    ...film,
+    characters: (film.characters || []).map((pId) => characters[pId]),
+    comments,
+  };
 };
 
-export const getCharacterById = async (characterId) => {
+export const getCharacterById = async (characterId: string) => {
   const films = await readFilms();
   const characters = await readCharacters();
   const character = characters[characterId];
   return {
     ...character,
-    films: character.films.map((filmId) => films[filmId]),
+    films: (character.films || []).map((filmId) => films[filmId]),
   };
 };
